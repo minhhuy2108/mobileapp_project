79842d3dfc3e86a5256a20f641d311e2490f6e5a
unknown <leminhhuy.work@gmail.com>
2024-06-20 03:09:53 +0700

    done HOMESCREEN

--- app/src/main/java/com/example/newsproject/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsproject/presentation/navgraph/NavGraph.kt
@@ -7,6 +7,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.newsproject.presentation.home.HomeScreen
+import com.example.newsproject.presentation.home.HomeViewModel
 import com.example.newsproject.presentation.onboarding.OnBoardingScreen
 import com.example.newsproject.presentation.onboarding.OnBoardingViewModel
 
@@ -34,7 +37,11 @@ fun NavGraph(
             startDestination = Route.NewsNavigatorScreen.route
         ){
             composable(route = Route.NewsNavigatorScreen.route){
-                Text(text = "News Navigator Screen")
+                val viewModel: HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = articles) {
+
+                }
             }
         }
     }
